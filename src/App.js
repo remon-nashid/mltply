9d8edc94d9c54e32452c2c4bd6420dfc92546c54
Remon Nashid <remon.sherin@gmail.com>
2018-06-19 21:09:59 -0700

    Add ejected (default) NB theme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 // @flow
 
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
-import { Text } from 'native-base'
+import { Container, Header, Content, Footer, StyleProvider } from 'native-base'
+import getTheme from '../native-base-theme/components'
+import material from '../native-base-theme/variables/material'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up src/App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <StyleProvider style={getTheme(material)}>
+        <Container>
+          <Header />
+          <Content />
+          <Footer />
+        </Container>
+      </StyleProvider>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
